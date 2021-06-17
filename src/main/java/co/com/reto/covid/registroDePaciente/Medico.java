712936dcbce2c3234de79adc174e999beeb99313
Juan carlos salcedo <juank9225@gmail.com>
2021-06-17 08:19:01 -0500

    Se crea la entidad medico la cual pertenece al agregado registro de paciente

--- src/main/java/co/com/reto/covid/registroDePaciente/Medico.java
+++ src/main/java/co/com/reto/covid/registroDePaciente/Medico.java
@@ -1,4 +1,56 @@
 package co.com.reto.covid.registroDePaciente;
 
-public class Medico {
+import co.com.reto.covid.registroDePaciente.values.*;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class Medico extends Entity<IdentificacionMedico> {
+
+    protected TipoDeIdentificacion tipoDeIdentificacion;
+    protected Nombres nombres;
+    protected Telefono telefono;
+    protected RegistroMedico registroMedico;
+    protected Especialidad especialidad;
+
+    public Medico(IdentificacionMedico identificacionMedico,TipoDeIdentificacion tipoDeIdentificacion,Nombres nombres,Telefono telefono,RegistroMedico registroMedico,Especialidad especialidad) {
+        super(identificacionMedico);
+        this.tipoDeIdentificacion = tipoDeIdentificacion;
+        this.nombres = nombres;
+        this.telefono = telefono;
+        this.registroMedico = registroMedico;
+        this.especialidad = especialidad;
+    }
+
+    public void actualizarNombreMedico(Nombres nombres){
+        this.nombres = Objects.requireNonNull(nombres);
+    }
+
+    public void actualizarTelefonoMedico(Telefono telefono){
+        this.telefono = Objects.requireNonNull(telefono);
+    }
+
+    public void actualizarEspecialidadMedico(Especialidad especialidad){
+        this.especialidad = Objects.requireNonNull(especialidad);
+    }
+
+    public TipoDeIdentificacion TipoDeIdentificacion() {
+        return tipoDeIdentificacion;
+    }
+
+    public Nombres Nombres() {
+        return nombres;
+    }
+
+    public Telefono Telefono() {
+        return telefono;
+    }
+
+    public RegistroMedico RegistroMedico() {
+        return registroMedico;
+    }
+
+    public Especialidad Especialidad() {
+        return especialidad;
+    }
 }
