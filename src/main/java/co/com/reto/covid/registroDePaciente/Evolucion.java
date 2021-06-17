5ba3b0a69de2431c418a15f61bb884de0d9a2830
Juan carlos salcedo <juank9225@gmail.com>
2021-06-17 02:37:49 -0500

    Se crea la entidad Evolucion la cual es administrada por el agregado registro de paciente

--- src/main/java/co/com/reto/covid/registroDePaciente/Evolucion.java
+++ src/main/java/co/com/reto/covid/registroDePaciente/Evolucion.java
@@ -1,11 +1,64 @@
 package co.com.reto.covid.registroDePaciente;
 
-import co.com.reto.covid.registroDePaciente.values.NumeroId;
+import co.com.reto.covid.registroDePaciente.values.*;
 import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
 
 public class Evolucion extends Entity <NumeroId>{
 
-    public Evolucion(NumeroId entityId) {
-        super(entityId);
+    protected Estado estado;
+    protected Temperatura temperatura;
+    protected SaturacionDeOxigeno saturacionDeOxigeno;
+    protected FrecuenciaRespiratoria frecuenciaRespiratoria;
+    protected Observacion observacion;
+
+    public Evolucion(NumeroId numeroId,Estado estado,Temperatura temperatura,SaturacionDeOxigeno saturacionDeOxigeno,FrecuenciaRespiratoria frecuenciaRespiratoria, Observacion observacion) {
+        super(numeroId);
+        this.estado = estado;
+        this.temperatura = temperatura;
+        this.saturacionDeOxigeno = saturacionDeOxigeno;
+        this.frecuenciaRespiratoria = frecuenciaRespiratoria;
+        this.observacion = observacion;
+    }
+
+    public void actualizarEstadoPaciente(Estado estado){
+        this.estado = Objects.requireNonNull(estado);
+    }
+
+    public void actualizarTemperaturaPaciente(Temperatura temperatura){
+        this.temperatura = Objects.requireNonNull(temperatura);
+    }
+
+    public void actualizarSaturacionDeOxigeno(SaturacionDeOxigeno saturacionDeOxigeno){
+        this.saturacionDeOxigeno = Objects.requireNonNull(saturacionDeOxigeno);
+    }
+
+    public void actualizarFrecuenciaRespiratoria(FrecuenciaRespiratoria frecuenciaRespiratoria){
+        this.frecuenciaRespiratoria = Objects.requireNonNull(frecuenciaRespiratoria);
+    }
+
+    public void actualizarObservacion(Observacion observacion){
+        this.observacion = Objects.requireNonNull(observacion);
+    }
+
+    public Estado Estado() {
+        return estado;
+    }
+
+    public Temperatura Temperatura() {
+        return temperatura;
+    }
+
+    public SaturacionDeOxigeno SaturacionDeOxigeno() {
+        return saturacionDeOxigeno;
+    }
+
+    public FrecuenciaRespiratoria FrecuenciaRespiratoria() {
+        return frecuenciaRespiratoria;
+    }
+
+    public Observacion Observacion() {
+        return observacion;
     }
 }
